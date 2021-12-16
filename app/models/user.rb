2766c2854f9f454cbefe32a65991940be3190af3
shinobum10 <snb025562@gmail.com>
2021-12-16 17:13:09 +0900

    テストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true 
-  validates :password, presence: true, length: { minimum: 6 }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
   validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/}
