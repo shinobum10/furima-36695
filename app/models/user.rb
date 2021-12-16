36f45203efa2e5e761dd2a15385c9a271aff3af4
shinobum10 <snb025562@gmail.com>
2021-12-16 12:32:55 +0900

    プルリクエストの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true 
+  validates :password, presence: true, length: { minimum: 6 }
   validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,11 @@ RSpec.describe User, type: :model do
         @user.password_confirmation = 's123456'
         expect(@user).to be_valid
       end
+      it "passwordが半角英数字であれば登録できる" do
+        @user.password = 's123456'
+        @user.password_confirmation = 's123456'
+        expect(@user).to be_valid
+      end
     end
     context '新規登録できない場合' do
       it "nicknameが空では登録できない" do
