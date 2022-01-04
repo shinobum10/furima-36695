d43ef457a54ea2cd2d61eeacfc14e2d55d7a2233
shinobum10 <snb025562@gmail.com>
2022-01-04 18:09:03 +0900

    ログアウトからトップ画面に遷移を実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,14 +1,10 @@
 window.addEventListener('load', () => {
-  console.log("OK");
   const priceInput = document.getElementById("item-price");
-  console.log(priceInput);
-  priceInput.addEventListener("input", () => {
+    priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
     const addProfitDom = document.getElementById("profit");
-    console.log(priceInput.innerHTML);
     addProfitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
   })
 })
