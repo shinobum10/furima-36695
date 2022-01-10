524bc3572c3a5542056bde8b46f61c16a939c849
shinobum10 <snb025562@gmail.com>
2022-01-10 11:24:23 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,11 +16,11 @@ class ItemsController < ApplicationController
     end
   end
   
-  # def destroy
-  #   @item = Item.find(params[:id])
-  #   @item.destroy
-  #   redirect_to root_path
-  #end
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
 
   def create
     @item = Item.new(item_params)
